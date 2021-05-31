72586200c762a18d11421cb6bcf1afeca2e661e3
Jeb-Cajayon <84359766+Jeb-Cajayon@users.noreply.github.com>
2021-05-31 22:17:36 +0800

    Add files via upload

---
+++ Report/Login.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Login</title>
+		<link rel="icon" href="pics\logo.png">
+		<link rel="stylesheet" href="css\Report.css">
+	</head>
+	<?php include 'header.php';?>
+</html>
---
+++ Report/header.php
@@ -0,0 +1,4 @@
+<div class="header">
+<img class="logo" src="pics\logo.png">
+<h2 class="headertext"> Spark Academy of Global City, Inc.</h2>
+<p class="headertext"> 4th st. GHQ Signal Village, Brgy. Katuparan, Taguig, 1632 Metro Manila</p>
