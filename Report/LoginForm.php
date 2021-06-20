fb1987b0e8841d971f22c909b378c8467ee294de
Matthew Sotelo <84297787+Matthew-Sotelo@users.noreply.github.com>
2021-06-20 20:06:38 +0800

    Add files via upload

---
+++ Report/LoginForm.php
@@ -0,0 +1,18 @@
+<div class="login-form">
+        <form class="login-credential">
+            <h1 class="login-header">Login</h1>
+            <div>
+                <input id="input-style" type="text" placeholder="Username">
+            </div>
+            <div>
+            	<input id="input-style" type="password" placeholder="Password">
+            </div>
+             <input class="login-button" type="submit" value="Login">
+             <div class="signup">
+                 Don't have an account?
+             </br>
+             <a href="registration.php">Sign up</a>
+             </div>
+			 <img id="poster" src="pics\poster.png">
+        </form>
+    </div>
