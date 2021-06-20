3c87b22b45c7cf7045510ed995e88992b3cebfbc
Matthew Sotelo <84297787+Matthew-Sotelo@users.noreply.github.com>
2021-06-20 20:16:37 +0800

    Update index.php

--- Report/index.php
+++ Report/index.php
@@ -4,8 +4,10 @@
 		<title>Login</title>
 		<link rel="icon" href="pics\logo.png">
 		<link rel="stylesheet" href="css\Report.css">
+		<link rel="stylesheet" href="css\login.css">
 	</head>
 	<body>
 	<?php include 'header.php';?>
+	<?php include 'LoginForm.php';?>
 	</body>
 </html>
