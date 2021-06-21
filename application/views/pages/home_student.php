65f2007a172acd6775c07549419a237de931305f
rambalois <84296721+rambalois@users.noreply.github.com>
2021-06-21 21:58:37 +0800

    update

--- application/views/pages/home_student.php
+++ application/views/pages/home_student.php
@@ -31,7 +31,7 @@
 			</li>
 			<li class="drag-column drag-column-needs-review">
 				<span class="drag-column-header">
-					<h2>For Review</h2>
+					<h2>Science</h2>
 					<svg data-target="options3" class="drag-header-more" fill="#FFFFFF" height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0z" fill="none"/><path d="M12 8c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2zm0 2c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2zm0 6c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2z"/></svg>
 				</span>
 				<div class="drag-options" id="options3"></div>
@@ -42,7 +42,7 @@
 			</li>
 			<li class="drag-column drag-column-approved">
 				<span class="drag-column-header">
-					<h2>Science</h2>
+					<h2>For Review</h2>
 					<svg data-target="options4" class="drag-header-more" fill="#FFFFFF" height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0z" fill="none"/><path d="M12 8c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2zm0 2c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2zm0 6c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2z"/></svg>
 				</span>
 				<div class="drag-options" id="options4"></div>
