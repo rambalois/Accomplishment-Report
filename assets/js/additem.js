d172f71964d8b220a44fb69fe28161bb9cab3519
keanueco <84165797+keanueco@users.noreply.github.com>
2021-06-21 18:44:57 +0800

    added upload button
    upload button added to board

--- assets/js/additem.js
+++ assets/js/additem.js
@@ -1,5 +1,5 @@
 function addItemToList(number) {
-	$("#"+number).append('<li ><textarea oncontextmenu="return false;" class="drag-item drag-textarea">New Item!</textarea></li>');
+	$("#"+number).append('<li ><textarea oncontextmenu="return false;" class="drag-item drag-textarea">New Item!</textarea><input type="file" id="myfile" name="myfile"></li>');
 }
 
 function addItemToListAsStudent(number) {
