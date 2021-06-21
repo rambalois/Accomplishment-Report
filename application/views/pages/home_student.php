0da36b26cc8cca6eed5153d1e8bfe752e94ef08f
rambalois <84296721+rambalois@users.noreply.github.com>
2021-06-21 21:55:54 +0800

    update

--- application/views/pages/home_student.php
+++ application/views/pages/home_student.php
@@ -7,7 +7,7 @@
 		<ul class="drag-list">
 			<li class="drag-column drag-column-on-hold">
 				<span class="drag-column-header">
-					<h2>To Do</h2>
+					<h2>English</h2>
 					<svg class="drag-header-more" data-target="options1" fill="#FFFFFF" height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0z" fill="none"/><path d="M12 8c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2zm0 2c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2zm0 6c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2z"/></svg>
 				</span>
 					
@@ -20,7 +20,7 @@
 			</li>
 			<li class="drag-column drag-column-in-progress">
 				<span class="drag-column-header">
-					<h2>In Progress</h2>
+					<h2>Math</h2>
 					<svg class="drag-header-more" data-target="options2" fill="#FFFFFF" height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0z" fill="none"/><path d="M12 8c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2zm0 2c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2zm0 6c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2z"/></svg>
 				</span>
 				<div class="drag-options" id="options2"></div>
@@ -42,7 +42,7 @@
 			</li>
 			<li class="drag-column drag-column-approved">
 				<span class="drag-column-header">
-					<h2>Checked</h2>
+					<h2>Science</h2>
 					<svg data-target="options4" class="drag-header-more" fill="#FFFFFF" height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0z" fill="none"/><path d="M12 8c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2zm0 2c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2zm0 6c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2z"/></svg>
 				</span>
 				<div class="drag-options" id="options4"></div>
