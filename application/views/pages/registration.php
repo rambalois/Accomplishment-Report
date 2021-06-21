6cb1665cfb1b3e8400ddf591f76362afba251fcc
keanueco <84165797+keanueco@users.noreply.github.com>
2021-06-21 14:46:49 +0800

    added favicon to registration
    added favicon to registration

--- application/views/pages/registration.php
+++ application/views/pages/registration.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-    
+    <link rel="shortcut icon" type="image/jpg" href="assets/img/favicon.ico"/>
      <!--  Meta tags -->
       <meta charset="utf-8">
       <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
